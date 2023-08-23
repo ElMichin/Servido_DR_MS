e71c3da5bd6c404383180f959f836894199c5db3
ElMichin <gimaga0428@gmail.com>
2023-08-22 23:07:32 -0500

    Se creo cliente
    Funciones del cliente para cpnectar y hablar con el servidor

--- src/co/edu/unbosque/model/Cliente.java
+++ src/co/edu/unbosque/model/Cliente.java
@@ -1,5 +1,69 @@
 package co.edu.unbosque.model;
 
-public class Cliente {
+	import java.io.DataInputStream;
+	import java.io.DataOutputStream;
+	import java.io.IOException;
+	import java.net.Socket;
+	import java.util.Scanner;
 
-}
+	public class Cliente extends Thread {
+		// initialize socket and input output streams
+		private int port = 8888;
+		private DataInputStream in;
+		private DataOutputStream out;
+		private String address; 
+
+		
+	 
+		// constructor to put ip address and port
+		public Cliente(int port, String address) {
+			// initialize socket and input output streams
+			
+			this.port = port;
+			this.address = address;
+			
+			
+
+
+		}
+
+		@Override
+		public void run() {
+			Scanner sn = new Scanner(System.in);
+			
+
+				try {
+					
+					ciclo1: while(true) {
+					Socket sk = new Socket("127.0.0.1", 8888);
+
+					this.out = new DataOutputStream(sk.getOutputStream());
+					this.in = new DataInputStream(sk.getInputStream());
+					
+					String mensajeCliente = sn.nextLine();
+					
+					this.out.writeUTF(mensajeCliente);
+					String respuestaBot = in.readUTF();
+					System.out.println("Bot" + respuestaBot);
+								
+					System.out.println("Received message:");
+					}
+					
+				} catch (IOException i) {
+					System.out.println(i);
+				}
+			
+			// close the connection
+			try {
+				out.close();
+				
+			} catch (IOException i) {
+				System.out.println(i);
+			}
+					
+
+		}
+
+		
+	}
+
